e243baa1fc98eb42aa8946cd49f7ea720bb7d863
walbornoz1 <87739177+walbornoz1@users.noreply.github.com>
2021-07-21 20:25:17 +0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,30 +1,37 @@
-## version 1, take the makevector as base
+## version 2, change mean for inverse
+## change x=numeric() for x=matrix()
+## m is the inverse now. "inversa" is in Spanish
+## the operator <-- save inverse in the parent environment, so
+##   the inverse value is safe
+## solve is the formula of inverse of an matrix
+## the logic of the program is the same 
 
-makeVector <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinversa <- function(inversa) m <<- inversa
+  getinversa <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinversa = setinversa,
+       getinversa = getinversa)
 }
 
-cachemean <- function(x, ...) {
-  m <- x$getmean()
+cacheSolve <- function(x, ...) {
+  m <- x$getinversa()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...)
+  x$setinversa(m)
   m
 }
+
 
 ##################################
 
